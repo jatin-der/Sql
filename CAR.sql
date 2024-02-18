f34bbb8a5fea016e6ad19e927bac2c014e37c5d8
Jatinder Kumar <140374551+jatin-der@users.noreply.github.com>
2024-02-18 22:21:38 +0530

    Add files via upload

--- CAR.sql
+++ CAR.sql
@@ -39,13 +39,36 @@ select year, count(*) from car_dekho where fuel='Diesel' group by year;
 select year, count(*) from car_dekho where fuel='cng' group by year;
 
 -- which year has more than 100 of cars;--
-select year,count()from car_dekho group by year having count()>100;
-select year,count()from car_dekho group by year having count()<50;
+select year,count(*)from car_dekho group by year having count(*)>100;
+select year,count(*)from car_dekho group by year having count(*)<50;
 
 -- all cars count details between 2015 and 2023--
 select count(*)from car_dekho where year between 2015 and 2023;
 
 -- all cars details between 2015 and 2023--
 select *from car_dekho where year between 2015 and 2023;
+select *from car_dekho;
+
+-- cars count year 2015 or 2014--
+select count(*)from car_dekho where year =2015 or 2014;
+
+-- need to find the car maruti alto and maruti lxi--
+select *from car_dekho where name in (' Maruti Alto 800 LXI Opt',' Maruti S-Presso LXi');
+select count(*)from car_dekho where name in (' Maruti Alto 800 LXI Opt',' Maruti S-Presso LXi'); #3
+
+-- need to find the cars except maruti alto and maruti lxi--
+select *from car_dekho where name not in (' Maruti Alto 800 LXI Opt',' Maruti S-Presso LXi');
+select count(*)from car_dekho where name not in (' Maruti Alto 800 LXI Opt',' Maruti S-Presso LXi'); #7924
+
+
+-- number of cars of maruti --
+select *from car_dekho where name like 'maruti%';
+select count(*)from car_dekho where name like 'maruti%';
+
+-- Details of car by name and price--
+select name,concat('Rs',selling_price) from car_dekho;
+select name,concat('Rs',selling_price),year from car_dekho;
+select name,concat('Rs',format(selling_price,0)) from car_dekho;
+
 
 -- End --
